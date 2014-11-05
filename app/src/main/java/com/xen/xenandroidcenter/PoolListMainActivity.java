8575a8fec525bb087b81aac51374373b4bda2c7e
Zheng Chai <zheng.chai@citrix.com>
2014-11-05 13:52:23 +0800

    Add the List view Signed-off-by: Zheng Chai <zheng.chai@citrix.com>

--- app/src/main/java/com/xen/xenandroidcenter/PoolListMainActivity.java
+++ app/src/main/java/com/xen/xenandroidcenter/PoolListMainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class PoolListMainActivity extends Activity {
@@ -11,7 +12,10 @@ public class PoolListMainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //self define window title
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_pool_list_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.cust_activity_title);
     }
 
 }
