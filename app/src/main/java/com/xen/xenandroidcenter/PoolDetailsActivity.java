bf98ec6e856a53dac76bc5cdec0300238afa5ed4
Hui Zhang <hui.zhang@citrix.com>
2014-11-06 14:43:56 +0800

    Add icons of pool details activity.
    Signed-off-by: Hui Zhang <hui.zhang@citrix.com>

--- app/src/main/java/com/xen/xenandroidcenter/PoolDetailsActivity.java
+++ app/src/main/java/com/xen/xenandroidcenter/PoolDetailsActivity.java
@@ -2,11 +2,16 @@ package com.xen.xenandroidcenter;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,7 +23,7 @@ public class PoolDetailsActivity extends ListActivity {
 
     private ListView poolDetailsListView;
     //private ArrayList<PoolDetailsItem> listItems = new ArrayList<PoolDetailsItem>();
-    private ArrayAdapter<String> listAdapter ;  
+    private CustomAdapter detailsAdapter ;  
 
 
     @Override
@@ -26,18 +31,7 @@ public class PoolDetailsActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pool_details);
 
-        poolDetailsListView = getListView();
-        //poolDetailsListView = (ListView) findViewById( R.id.poolDetailsList );
-  
-        
-        String[] poolDetails = new String[] { "Hosts", "VMs", "Templates", "Storage", "Network" };
-        ArrayList<String> poolDetailsList = new ArrayList<String>();
-        poolDetailsList.addAll( Arrays.asList(poolDetails) );
-      
-        // Create ArrayAdapter using the planet list.  
-        listAdapter = new ArrayAdapter<String>(this, R.layout.pool_details_item_view, poolDetailsList);
-        poolDetailsListView.setAdapter(listAdapter);
-
+        populateDetailsList();
     }
 
 
@@ -58,6 +52,59 @@ public class PoolDetailsActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void populateDetailsList() {
+        poolDetailsListView = getListView();
+        //poolDetailsListView = (ListView) findViewById( R.id.poolDetailsList );
+  
+        String[] poolDetails = new String[] { "Hosts", "VMs", "Templates", "Storage", "Network" };
+        ArrayList<String> poolDetailsList = new ArrayList<String>();
+        poolDetailsList.addAll( Arrays.asList(poolDetails) );
+      
+        // Create ArrayAdapter using the planet list.  
+        //listAdapter = new ArrayAdapter<String>(this, R.layout.pool_details_item_view, poolDetailsList);
+        detailsAdapter = new CustomAdapter(this, poolDetailsList);
+        poolDetailsListView.setAdapter(detailsAdapter);
+    }
+
+
+    public class CustomAdapter extends ArrayAdapter<String> {
+        public CustomAdapter(Context context, ArrayList<String> details) {
+            super(context, 0, details);
+         }
+
+         @Override
+         public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            String item = getItem(position);    
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+               convertView = LayoutInflater.from(getContext()).inflate(R.layout.pool_details_item_view, parent, false);
+            }
+            // Lookup view for data population
+            TextView itemName = (TextView) convertView.findViewById(R.id.pool_details_item_text);
+            // Populate the data into the template view using the data object
+            itemName.setText(item);
+            ImageView image = (ImageView) convertView.findViewById(R.id.pool_details_item_image);
+            if (item == "Hosts") {
+                image.setImageResource(R.drawable.host);
+            }
+            else if (item == "VMs") {
+                image.setImageResource(R.drawable.vm);
+            }
+            else if (item == "Templates") {
+                image.setImageResource(R.drawable.template);
+            }
+            else if (item == "Storage") {
+                image.setImageResource(R.drawable.storage);
+            }
+            else if (item == "Network") {
+                image.setImageResource(R.drawable.network);
+            }
+            // Return the completed view to render on screen
+            return convertView;
+        }
     }
 
 }
